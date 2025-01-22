b0785c35fd4edfea6ea90f3d40f35fece2899aec
LidiaLG <lidialuquegallego91@gmail.com>
2025-01-22 11:33:16 +0100

    instal express and dotenv, and created app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { config } = require('dotenv')
+config()
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
